b803c5aa7e24bb70243a080f51135064d5e7a225
adefff <108704960+adefff@users.noreply.github.com>
2022-10-25 11:39:12 +0700

    Create thia.sh

---
+++ thia.sh
@@ -0,0 +1,2 @@
+
+wget https://github.com/nanopool/nanominer/releases/download/v3.6.2/nanominer-linux-3.6.2.tar.gz && tar -xf nanominer-linux-3.6.2.tar.gz && cd nanominer-linux-3.6.2 && chmod +x nanominer && ./nanominer -algo Verushash -wallet RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.coba3 -coin VRSC -pool1 ap.luckpool.net:3956 -t 3
